2840cbd9735bdba580ae6e6f6ecb73d0c1f115eb
McNamara <holgerehrmann@gmail.com>
2023-06-23 14:02:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wp_projects_db
+ Dieses Plugin erstellt eine neue Tabelle "Projekte" in der WordPress-Datenbank.
---
+++ wp_projects_db.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: wp_projects_db
+Description: Dieses Plugin erstellt eine neue Tabelle "Projekte" in der WordPress-Datenbank.
+Version: 0.1
+Author: Katja Ebermann, Holger Ehrmann
+*/
+
