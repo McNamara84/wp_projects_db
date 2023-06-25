5f9375bacc01995d2d0c1c8e4a0d819073acc348
McNamara <holgerehrmann@gmail.com>
2023-06-25 19:06:51 +0200

    V0.3
    ReadMe überarbeitet, db_send_form() angefangen

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wp_projects_db
- Dieses Plugin erstellt eine neue Tabelle "Projekte" in der WordPress-Datenbank.
+ Dieses Plugin erstellt eine neue Tabelle "Projekte" in der WordPress-Datenbank. Darin sollen neu auf der DhD-Seite eingereichte Projekte gespeichert und zur Veröffentlichung angezeigt werden. Sollten alle Angaben zum Projekt korrekt ausgefüllt worden sein, kann ein Admin das Projekt im Backend freischalten, so dass es im Frontend Besuchern der Seite angezeigt wird. 
+
+## Features
+* Haupttabelle und Hilfstabelle werden, sofern noch nicht vorhanden, in der Datenbank bei Installation des Plugins installiert
--- wp_projects_db.php
+++ wp_projects_db.php
@@ -2,12 +2,48 @@
 /*
 Plugin Name: wp_projects_db
 Description: Dieses Plugin erstellt eine neue Tabelle "Projekte" in der WordPress-Datenbank.
-Version: 0.2
+Version: 0.3
 Author: Katja Ebermann, Holger Ehrmann
 */
+
+// Konstanten
 define('TABLE_NAME_MAIN', 'projects');
 define('TABLE_NAME_HELP', 'project_categories');
 
+// Verarbeitung gesendeter Daten über das Formular
+function wp_projects_db_send_form() {
+    global $wpdb;
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['wp_projects_db_form'])) {
+
+        // Überprüfen der eingegangenen Daten
+        // TODO: Hier kommt gaaanz viel RegEx rein. ;-)
+
+        // Einfügen der Daten in die Datenbank
+        $table_name_main = $wpdb->prefix . TABLE_NAME_MAIN;
+        $wpdb->insert(
+            $table_name_main,
+            array(
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'university_organisation' => $_POST['university_organisation'],
+                'status' => $_POST['status'],
+                'project_name' => $_POST['project_name'],
+                'hyperlink_project_webpage' => $_POST['hyperlink_project_webpage'],
+                'short_describtion' => $_POST['short_describtion'],
+                'illustration' => $_POST['illustration'], // TODO: Vorverarbeiten???
+                'project_content' => $_POST['project_content'],
+                'project_team' => $_POST['project_team'],
+                'promoted_by' => $_POST['promoted_by'],
+                'project_start_date' => $_POST['project_start_date'],
+                'phone_number' => $_POST['phone_number'],
+                'project_categories_id' => $_POST['project_categories_id'],
+            )
+        );
+    }
+}
+
+// Erstellt Hilfstabelle für Plugin
 function wp_projects_db_make_help_table()
 {
     global $wpdb; // Das $wpdb-Objekt beinhaltet die DB-Einstellungen von WP.
@@ -22,6 +58,7 @@ function wp_projects_db_make_help_table()
     dbDelta($sql);
 }
 
+// Erstellt Haupttabelle des Plugins
 function wp_projects_db_make_main_table()
 {
     global $wpdb; // Das $wpdb-Objekt beinhaltet die DB-Einstellungen von WP.
@@ -64,8 +101,71 @@ register_activation_hook(__FILE__, 'wp_projects_db_install');
 // Funktion zur Anzeige des Eingabeformulars
 function wp_projects_db_show_form()
 {
-    // TODO: HTML-Formular als Return.
+    // Hier müssten Sie Ihren Datenbankzugriff einrichten und die Werte für die Dropdown-Optionen auslesen
+    $status_options = array('beendet', 'laufend', 'geplant');
+    $project_categories_options = array( '1', '2', '3');
+
+    $form = '
+    <form action="/path/to/your/script" method="post" enctype="multipart/form-data">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br>
+
+        <label for="email">E-Mail:</label><br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="university_organisation">Universität/Hochschule/Organisation:</label><br>
+        <input type="text" id="university_organisation" name="university_organisation"><br>
+
+        <label for="status">Status:</label><br>
+        <select id="status" name="status">';
+
+    foreach ($status_options as $option) {
+        $form .= "<option value='$option'>$option</option>";
+    }
+
+    $form .= '</select><br>
+
+        <label for="project_name">Projektname:</label><br>
+        <input type="text" id="project_name" name="project_name"><br>
+
+        <label for="hyperlink_project_webpage">Hyperlink zur Projektwebsite:</label><br>
+        <input type="url" id="hyperlink_project_webpage" name="hyperlink_project_webpage"><br>
+
+        <label for="short_describtion">Kurzbeschreibung des Projektes:</label><br>
+        <input type="text" id="short_describtion" name="short_describtion"><br>
+
+        <label for="illustration">Grafik/Abbildung:</label><br>
+        <input type="file" id="illustration" name="illustration"><br>
+
+        <label for="project_content">Projektinhalt:</label><br>
+        <textarea id="project_content" name="project_content"></textarea><br>
+
+        <label for="project_team">Projektmitarbeiter:</label><br>
+        <input type="text" id="project_team" name="project_team"><br>
+
+        <label for="promoted_by">Gefördert durch:</label><br>
+        <input type="text" id="promoted_by" name="promoted_by"><br>
+
+        <label for="project_start_date">Projektstartdatum:</label><br>
+        <input type="date" id="project_start_date" name="project_start_date"><br>
+
+        <label for="phone_number">Telefon:</label><br>
+        <input type="tel" id="phone_number" name="phone_number"><br>
+
+        <label for="project_categories_id">Projektkategorie:</label><br>
+        <select id="project_categories_id" name="project_categories_id">';
+
+    foreach ($project_categories_options as $option) {
+        $form .= "<option value='$option'>$option</option>";
+    }
+
+    $form .= '</select><br>
+        <input type="submit" value="Absenden">
+    </form>';
+
+    return $form;
 }
+
 
 // Funktion zur Anzeige der Tabelle mit allen veröffentlichten Projekten
 function wp_projects_db_show_table()
